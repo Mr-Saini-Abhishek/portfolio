ce44b1000406c9e5954be49d0480eba4987b0dc6
Mr. <elvishbaba282+github@gmail.com>
2024-05-31 16:54:31 +0530

    some changes

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
@@ -11,11 +10,8 @@ import {
   DiGithub,
   DiMysql,
   DiNpm,
-  DiMaterializecss
 } from "react-icons/di";
-import {
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+
 
 function Techstack() {
   return (
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { SiVisualstudiocode, SiPostman, SiMacos, SiWindows, SiOpenai } from "react-icons/si";
+import { SiVisualstudiocode, SiPostman, SiWindows, SiOpenai } from "react-icons/si";
 
 function Toolstack() {
   return (
